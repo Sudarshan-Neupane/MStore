78ad09f9c2ab533eb3f5031a84c55d9569053a42
bipinstha <aakee.stha@gmail.com>
2015-07-10 23:54:13 -0500

    # test configuration

---
+++ src/test/java/mum/edu/mstore/test/BaseTest.java
@@ -0,0 +1,11 @@
+package mum.edu.mstore.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/spring/servlet-test-context.xml"})
+public abstract class BaseTest {
+
+}
---
+++ src/test/java/mum/edu/mstore/test/UserTest.java
@@ -0,0 +1,34 @@
+package mum.edu.mstore.test;
+
+
+public class UserTest extends BaseTest {
+	// @Autowired
+	// private UserService userService;
+	private static final String FIRST_NAME = "Sudarshan";
+	private static final String MIDDLE_NAME = "";
+	private static final String LAST_NAME = "Neupane";
+	private static final String USER_NAME = "sneupane@gmail.com";
+	private static final String PASSWORD = "nepal@123";
+	private static final String RE_PASSWORD = "nepal@123";
+	private static final String MOBILE = "3196140233L";
+	private static final String ADDRESS1 = "1000N 4th STREET";
+	private static final String ADDRESS2 = "";
+	private static final String CITY = "FAIRFIELD";
+	private static final String STATE = "IOWA";
+	private static final String ZIP_CODE = "52557";
+	// private static final String COUNTRY = "United State of America";
+
+	private static final String CHANGED_LAST_NAME = "Neupane JI";
+
+	// @Before
+	public void addUserTest() {
+		// TODO implement user add test
+		// Assert.assertEquals(USER_NAME, u.getUserName());
+	}
+
+	// @Test
+	public void updateUserTest() {
+		// TODO implement user update test
+		// Assert.assertEquals(CHANGED_LAST_NAME, u.getLastName());
+	}
+}
