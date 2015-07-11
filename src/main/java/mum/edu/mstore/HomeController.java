6ed8c7f94f8f24660967bdc67e3004dbc4ad112a
Nuhendra Giri <girinuhendra@gmail.com>
2015-07-11 13:59:58 -0500

    Changes in InternalViewResolver. Bug Fixed

--- src/main/java/mum/edu/mstore/HomeController.java
+++ src/main/java/mum/edu/mstore/HomeController.java
@@ -17,23 +17,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**git config --global core.excludesfile ~/.gitignore_global
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String getHome()
+	{
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		System.out.println("inside Home");
 		return "home";
 	}
+	
+	
 	
 }
