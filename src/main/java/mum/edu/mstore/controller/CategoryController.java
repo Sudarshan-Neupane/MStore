dc1407a70a56279a2dbdab47c4d097006f3f55b5
bipinstha <aakee.stha@gmail.com>
2015-07-13 22:11:05 -0500

    ###

--- src/main/java/mum/edu/mstore/controller/CategoryController.java
+++ src/main/java/mum/edu/mstore/controller/CategoryController.java
@@ -1,15 +1,15 @@
 package mum.edu.mstore.controller;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 import mum.edu.mstore.domain.Category;
 import mum.edu.mstore.domain.SubCategory;
 import mum.edu.mstore.service.CategoryService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
