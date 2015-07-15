488f3305efa899ca021c797a0ebe4e605afb5193
mesudarshan7 <mesudarshan7@gmail.com>
2015-07-14 22:33:51 -1000

    user aspect and add to cart added

---
+++ src/main/java/mum/edu/mstore/aspect/UserAspect.java
@@ -0,0 +1,22 @@
+package mum.edu.mstore.aspect;
+
+import java.util.Date;
+import java.util.logging.Logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ *
+ * @author sudarshan
+ */
+@Aspect
+public class UserAspect {
+  static Logger logger = Logger.getLogger(EmailAspect.class.getName());
+  @After("execution(* mum.edu.mstore.controller.HomeController.home(..))")  
+  public void logininfo(){
+      System.out.println("Error login page details");
+    logger.info("log file");  
+  }  
+}
